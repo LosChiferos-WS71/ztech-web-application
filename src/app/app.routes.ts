79c4b57e49d14ec9dc0232e7200476c6529c8448
marzzioChicana <u202022228@upc.edu.pe>
2024-05-24 16:13:21 -0500

    add login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { LoginFormComponent } from './account/components/login-form/login-form.component';
+import { LoginComponent } from './public/pages/login/login.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/login', pathMatch: 'full'},
-    {path: 'login', component: LoginFormComponent}
+    {path: 'login', component: LoginComponent}
 ];
