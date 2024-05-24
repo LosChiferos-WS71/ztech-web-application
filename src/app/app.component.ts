4fd94e2245dea61aab06b199d6edf47e525991d9
marzzioChicana <u202022228@upc.edu.pe>
2024-05-24 00:09:06 -0500

    add add-pot-input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { AddPotInputComponent } from './public/components/add-pot-input/add-pot-input.component';
+import { AddPotInputComponent } from './flowerpot/components/add-pot-input/add-pot-input.component';
 
 @Component({
   selector: 'app-root',
---
+++ src/app/flowerpot/components/add-pot-input/add-pot-input.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-add-pot-input',
+  standalone: true,
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  templateUrl: './add-pot-input.component.html',
+  styleUrl: './add-pot-input.component.css'
+})
+export class AddPotInputComponent {
+  input1Value: string = '';
+  input2Value: string = '';
+  input3Value: string = '';
+  input4Value: string = '';
+  input5Value: string = '';
+
+  soloNumeroLetras(event: any, inputField: 'input1Value' | 'input2Value' | 'input3Value' | 'input4Value' | 'input5Value') {
+    const inputValue = event.key;
+    const isLetterOrNumber: boolean = /^[0-9a-zA-Z]*$/.test(inputValue);
+    const currentValue = this[inputField];
+
+    if (!isLetterOrNumber || currentValue.length >= 1) {
+        event.preventDefault();
+    } else {
+        this[inputField] += inputValue.toUpperCase();
+    }
+  }
+}
