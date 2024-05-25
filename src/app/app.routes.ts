5126f04849d68166532e3dc4608872b66decab2e
marzzioChicana <u202022228@upc.edu.pe>
2024-05-25 11:59:18 -0500

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { AddPotComponent } from './public/pages/add-pot/add-pot.component';
 import { CodeConfirmationComponent } from './public/pages/code-confirmation/code-confirmation.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '', pathMatch: 'full' },
 	{ path: 'add/pot', component: AddPotComponent },
     { path: 'code/confirmation/:message', component: CodeConfirmationComponent}
 ];
