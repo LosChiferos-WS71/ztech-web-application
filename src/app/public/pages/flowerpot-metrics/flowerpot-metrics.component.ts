2c4cca22578bfdfae1f70e2acb69e64c6ad7b0d8
Aldo Francisco Pastrana Leon <U20211C186@upc.edu.pe>
2024-05-25 00:05:18 -0500

    Feat (add): Add FlowerpotMetricsComponent

---
+++ src/app/public/pages/flowerpot-metrics/flowerpot-metrics.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { ToolbarComponent } from '../../components/toolbar/toolbar.component';
+import { PlantStatusComponent } from '../../../flowerpot/components/plant-status/plant-status.component';
+import { PlantConditionDashboardComponent } from '../../../iot-solution/components/plant-condition-dashboard/plant-condition-dashboard.component';
+import {MatIconModule} from '@angular/material/icon';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-flowerpot-metrics',
+  standalone: true,
+  imports: [ToolbarComponent,PlantStatusComponent,PlantConditionDashboardComponent,MatIconModule],
+  templateUrl: './flowerpot-metrics.component.html',
+  styleUrl: './flowerpot-metrics.component.css'
+})
+export class FlowerpotMetricsComponent {
+  constructor(private router: Router) {}
+
+  onBack(): void {
+    this.router.navigate(['/']); // Asegúrate de tener la ruta correcta
+  }
+}
