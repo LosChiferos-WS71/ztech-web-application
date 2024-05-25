3514f0ed60f74215c2b6a14e501567e995927da6
Aldo Francisco Pastrana Leon <U20211C186@upc.edu.pe>
2024-05-25 00:00:40 -0500

    Feat (add): Add PlantStatusComponent

--- src/app/flowerpot/components/plant-status/plant-status.component.ts
+++ src/app/flowerpot/components/plant-status/plant-status.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 @Component({
   selector: 'app-plant-status',
   standalone: true,
-  imports: [MatCardModule,MatIconModule],
+  imports: [MatCardModule,MatIconModule,CommonModule],
   templateUrl: './plant-status.component.html',
   styleUrl: './plant-status.component.css'
 })
 export class PlantStatusComponent {
+  values = {
+    temperature: 30,
+    humidity: 75,
+    uv: 55
+  };
 
+  getColor(value: number): string {
+    if (value >= 70) {
+      return 'green';
+    } else if (value >= 50) {
+      return 'red';
+    } else {
+      return 'black';
+    }
+  }
 }
