25cdcf36c9bf948789f2d1531fce73f1aa454ea0
marzzioChicana <u202022228@upc.edu.pe>
2024-05-25 21:55:42 -0500

    add recover-password-form

---
+++ src/app/account/components/recover-password-form/recover-password-form.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+
+@Component({
+  selector: 'app-recover-password-form',
+  standalone: true,
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule],
+  templateUrl: './recover-password-form.component.html',
+  styleUrl: './recover-password-form.component.css'
+})
+export class RecoverPasswordFormComponent {
+  email: string = '';
+
+  constructor(private router: Router) {};
+
+  recoverPassword() {
+    // Logica de verificacion de correo y envio de correo
+    this.router.navigate(['/login']);
+  }
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { RecoverPasswordFormComponent } from './account/components/recover-password-form/recover-password-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/recover/password', pathMatch: 'full'},
+    {path: 'recover/password', component: RecoverPasswordFormComponent},
+];
