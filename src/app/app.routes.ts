f02ba75e0ca9b0da92d62407570962c66ada2b74
Jeremy_134 <u202022237@upc.edu.pe>
2024-05-25 19:46:51 -0500

    feat: add loaded-plant component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { ChoosePlantComponent } from './public/pages/choose-plant/choose-plant.component';
+import { LoadedPlantComponent } from './public/pages/loaded-plant/loaded-plant.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'choose-plant', pathMatch: 'full' },
-    { path: 'choose-plant', component: ChoosePlantComponent }
+    { path: 'choose-plant', component: ChoosePlantComponent },
+    { path: 'loaded-plant', component: LoadedPlantComponent}
 ];
---
+++ src/app/public/pages/loaded-pot/loaded-pot.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loaded-pot',
+  standalone: true,
+  imports: [],
+  templateUrl: './loaded-pot.component.html',
+  styleUrl: './loaded-pot.component.css'
+})
+export class LoadedPotComponent {
+
+}
