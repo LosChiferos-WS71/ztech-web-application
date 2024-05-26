32b107fc938cab12920ff2dde684f9bbd3879ff6
Sebastian Ramirez <u20201c031@upc.edu.pe>
2024-05-26 15:48:11 -0500

    feat(update): Update styles

--- src/app/flowerpot/components/search-plant/search-plant.component.ts
+++ src/app/flowerpot/components/search-plant/search-plant.component.ts
@@ -19,7 +19,7 @@ export class SearchPlantComponent {
   searchText = '';
   plants = [
     { name: 'PLANT 1', description: 'Plant 1', image: 'https://img.freepik.com/vector-gratis/planta-maceta-dibujos-animados_1308-107212.jpg' },
-    { name: 'PLANT 2', description: 'Plant 2', image: 'https://content.elmueble.com/medio/2019/03/25/monstera_f97f4746_800x800.jpg' },
+    { name: 'PLANT 1', description: 'Plant 2', image: 'https://content.elmueble.com/medio/2019/03/25/monstera_f97f4746_800x800.jpg' },
     { name: 'PLANT 3', description: 'Plant 3', image: 'https://media.istockphoto.com/id/1372896722/es/foto/planta-de-pl%C3%A1tano-en-maceta-aislada-sobre-fondo-blanco.jpg?s=612x612&w=0&k=20&c=l4EmNUhwxmge4YwY_u-n5YvOgzBjc4ruDGVfrm9m_s8='},
     { name: 'PLANT 4', description: 'Plant 4', image: 'https://img.freepik.com/vector-gratis/planta-maceta-dibujos-animados_1308-107212.jpg' },
     { name: 'PLANT 5', description: 'Plant 5', image: 'https://content.elmueble.com/medio/2019/03/25/monstera_f97f4746_800x800.jpg' },
@@ -43,4 +43,5 @@ export class SearchPlantComponent {
       plant.description.toLowerCase().includes(this.searchText.toLowerCase())
     );
   }
+
 }
