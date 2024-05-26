3b70079dde2e839092eec96576acf23ff7000b25
Jeremy_134 <u202022237@upc.edu.pe>
2024-05-25 20:39:59 -0500

    feat: add add-pot component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { ChoosePlantComponent } from './public/pages/choose-plant/choose-plant.component';
 import { LoadedPlantComponent } from './public/pages/loaded-plant/loaded-plant.component';
+import { AddPotComponent } from './public/pages/add-pot/add-pot.component';
+import { LoadedPotComponent } from './public/pages/loaded-pot/loaded-pot.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'choose-plant', pathMatch: 'full' },
     { path: 'choose-plant', component: ChoosePlantComponent },
-    { path: 'loaded-plant', component: LoadedPlantComponent}
+    { path: 'loaded-plant', component: LoadedPlantComponent},
+    { path: 'add-pot', component: AddPotComponent},
+    { path: 'loaded-pot', component: LoadedPotComponent}
 ];
--- src/app/public/pages/add-pot/add-pot.component.ts
+++ src/app/public/pages/add-pot/add-pot.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ToolbarComponent } from '../../components/toolbar/toolbar.component';
 
 @Component({
   selector: 'app-add-pot',
   standalone: true,
-  imports: [],
+  imports: [ToolbarComponent],
   templateUrl: './add-pot.component.html',
   styleUrl: './add-pot.component.css'
 })
