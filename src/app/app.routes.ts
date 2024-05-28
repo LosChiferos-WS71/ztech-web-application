e916ccd79a484db3cdb26f45fca83584187bbe76
marzzioChicana <u202022228@upc.edu.pe>
2024-05-28 16:25:28 -0500

    add edit-profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { EditProfileFormComponent } from './account/components/edit-profile-form/edit-profile-form.component';
 import { ProfileComponent } from './public/pages/profile/profile.component';
+import { EditProfileComponent } from './public/pages/edit-profile/edit-profile.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'profile', pathMatch: 'full' },
     { path: 'profile', component: ProfileComponent },
-    { path: 'edit/profile', component: EditProfileFormComponent}
+    { path: 'edit/profile', component: EditProfileComponent}
 ];
---
+++ src/app/public/pages/edit-profile/edit-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { ToolbarComponent } from '../../components/toolbar/toolbar.component';
+import { EditProfileFormComponent } from '../../../account/components/edit-profile-form/edit-profile-form.component';
+
+@Component({
+  selector: 'app-edit-profile',
+  standalone: true,
+  imports: [ToolbarComponent, EditProfileFormComponent],
+  templateUrl: './edit-profile.component.html',
+  styleUrl: './edit-profile.component.css'
+})
+export class EditProfileComponent {
+
+}
