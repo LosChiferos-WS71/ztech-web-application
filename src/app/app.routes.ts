f667b439ce9842d5bd9d8e83bef0a99f9e2f8d3f
marzzioChicana <u202022228@upc.edu.pe>
2024-05-28 16:02:08 -0500

    add profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { EditProfileFormComponent } from './account/components/edit-profile-form/edit-profile-form.component';
-import { ProfileFormComponent } from './account/components/profile-form/profile-form.component';
+import { ProfileComponent } from './public/pages/profile/profile.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'edit/profile', pathMatch: 'full' },
-    { path: 'profile', component: ProfileFormComponent },
+    { path: '', redirectTo: 'profile', pathMatch: 'full' },
+    { path: 'profile', component: ProfileComponent },
     { path: 'edit/profile', component: EditProfileFormComponent}
 ];
