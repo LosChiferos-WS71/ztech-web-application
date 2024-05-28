85da96754978e6497c52160c77ac7dfdd92e1393
marzzioChicana <u202022228@upc.edu.pe>
2024-05-27 21:55:41 -0500

    update profile-form

--- src/app/account/components/profile-form/profile-form.component.ts
+++ src/app/account/components/profile-form/profile-form.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-profile-form',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './profile-form.component.html',
   styleUrl: './profile-form.component.css'
 })
@@ -18,4 +20,10 @@ export class ProfileFormComponent {
   birthdate: string = '';
   gender: string = '';
   phone: string = '';
+
+  constructor(private router: Router) {}
+
+  editProfile(): void {
+    this.router.navigate(['edit/profile']);
+  }
 }
