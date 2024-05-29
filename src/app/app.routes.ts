0b8182f69a068e913b8afa337e2a5f3162156f27
Sebastian Ramirez <u20201c031@upc.edu.pe>
2024-05-28 23:16:38 -0500

    feat(update): Update route of toolbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { PlantDetailComponent } from './flowerpot/components/plant-detail/plant-
 import { ViewPlantComponent } from './public/pages/view-plant/view-plant.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'plant/view', pathMatch: 'full' },
     { path: 'plant/view', component: ViewPlantComponent },
     { path: 'plant/detail/:id', component: PlantDetailComponent },
 ];
--- src/app/public/components/toolbar/toolbar.component.ts
+++ src/app/public/components/toolbar/toolbar.component.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { ViewPlantComponent } from '../../pages/view-plant/view-plant.component';
 
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink, ViewPlantComponent],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css'
 })
