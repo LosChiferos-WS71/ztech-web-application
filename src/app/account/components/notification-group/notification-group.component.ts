3f92bf36f0a3aa3e8c3c795c7b83d284e5f2fdec
JosueCarrascoH <89035938+JosueCarrascoH@users.noreply.github.com>
2024-05-28 23:50:54 -0500

    feat: update notification card

--- src/app/account/components/notification-group/notification-group.component.ts
+++ src/app/account/components/notification-group/notification-group.component.ts
@@ -17,13 +17,13 @@ interface Notification {
 })
 export class NotificationGroupComponent implements OnInit{
     @Input() notifications: Notification[] = [
-        { id: '1', title: 'PLANT 1', plant: 'Plant 1', day: '2024-05-28' },
-        { id: '2', title: 'PLANT 1', plant: 'Plant 2', day: '2024-05-20' },
-        { id: '3', title: 'PLANT 3', plant: 'Plant 3', day: '2024-05-20' },
-        { id: '4', title: 'PLANT 4', plant: 'Plant 4', day: '2024-05-21' },
-        { id: '5', title: 'PLANT 5', plant: 'Plant 5', day: '2024-05-21' },
-        { id: '6', title: 'PLANT 6', plant: 'Plant 6', day: '2024-05-21' },
-        { id: '7', title: 'Regar Planta', plant: 'Plant 7', day: '2024-05-21' }
+        { id: '1', title: 'NOTIFICATION 1', plant: 'Plant 1', day: '2024-05-28' },
+        { id: '2', title: 'NOTIFICATION 1', plant: 'Plant 2', day: '2024-05-20' },
+        { id: '3', title: 'NOTIFICATION 3', plant: 'Plant 3', day: '2024-05-20' },
+        { id: '4', title: 'NOTIFICATION 4', plant: 'Plant 4', day: '2024-05-21' },
+        { id: '5', title: 'NOTIFICATION 5', plant: 'Plant 5', day: '2024-05-21' },
+        { id: '6', title: 'NOTIFICATION 6', plant: 'Plant 6', day: '2024-05-21' },
+        { id: '7', title: 'NOTIFICATION 7', plant: 'Plant 7', day: '2024-05-21' }
     ];
     groupedNotifications: { date: string; notifications: Notification[] }[] = [];
     hasNotifications = true;
