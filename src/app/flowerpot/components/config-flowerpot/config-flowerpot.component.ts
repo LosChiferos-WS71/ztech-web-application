3e9a22bd56391a8da20a65fd65fc0a4161a1c2a6
Jeremy_134 <u202022237@upc.edu.pe>
2024-05-29 11:59:30 -0500

    fix style

--- src/app/flowerpot/components/config-flowerpot/config-flowerpot.component.ts
+++ src/app/flowerpot/components/config-flowerpot/config-flowerpot.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-config-flowerpot',
@@ -9,4 +10,12 @@ import { Component } from '@angular/core';
 })
 export class ConfigFlowerpotComponent {
 
+  constructor(private router: Router) {}
+
+  onSaveClick() {
+    this.router.navigate(['/loaded/pot']);
+    setTimeout(() => {
+      this.router.navigate(['/choose/plant']);
+    }, 3000);
+  }
 }
--- src/app/public/pages/configuration-flowerpot/configuration-flowerpot.component.ts
+++ src/app/public/pages/configuration-flowerpot/configuration-flowerpot.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ToolbarComponent } from '../../components/toolbar/toolbar.component';
 import { Router } from '@angular/router';
+import { ConfigFlowerpotComponent } from '../../../flowerpot/components/config-flowerpot/config-flowerpot.component';
 
 @Component({
   selector: 'app-configuration-flowerpot',
   standalone: true,
-  imports: [ToolbarComponent],
+  imports: [ToolbarComponent, ConfigFlowerpotComponent],
   templateUrl: './configuration-flowerpot.component.html',
   styleUrl: './configuration-flowerpot.component.css'
 })
