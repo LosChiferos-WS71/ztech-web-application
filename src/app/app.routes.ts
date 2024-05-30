3eae8c204ced437c67cd4bfa658a405686400e0c
marzzioChicana <u202022228@upc.edu.pe>
2024-05-30 18:19:31 -0500

    add welcome-user

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { FlowerpotsListComponent } from './public/pages/flowerpots-list/flowerpots-list.component';
+import { WelcomeUserComponent } from './account/components/welcome-user/welcome-user.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/flowerpots/list', pathMatch: 'full' },
+    { path: '', redirectTo: '/welcome/user', pathMatch: 'full' },
+    { path: 'welcome/user', component: WelcomeUserComponent},
     { path: 'flowerpots/list', component: FlowerpotsListComponent}
 ];
