2f8b47d72ca0690004ea3f7908799670043eb419
marzzioChicana <u202022228@upc.edu.pe>
2024-05-29 23:10:28 -0500

    add card-flowerpot

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { CardFlowerpotComponent } from './flowerpot/components/card-flowerpot/card-flowerpot.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/card/flowerpot', pathMatch: 'full' },
+    { path: 'card/flowerpot', component: CardFlowerpotComponent}
+];
