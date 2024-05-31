6ad98a36ac355f118ffd6db055a6da7bbace6b16
marzzioChicana <u202022228@upc.edu.pe>
2024-05-30 19:04:19 -0500

    update flowerpots-list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,6 @@
 import { Routes } from '@angular/router';
 import { FlowerpotsListComponent } from './public/pages/flowerpots-list/flowerpots-list.component';
-import { WelcomeUserComponent } from './account/components/welcome-user/welcome-user.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/welcome/user', pathMatch: 'full' },
-    { path: 'welcome/user', component: WelcomeUserComponent},
     { path: 'flowerpots/list', component: FlowerpotsListComponent}
 ];
--- src/app/public/pages/flowerpots-list/flowerpots-list.component.ts
+++ src/app/public/pages/flowerpots-list/flowerpots-list.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
 import { ToolbarComponent } from '../../components/toolbar/toolbar.component';
+import { WelcomeUserComponent } from '../../../account/components/welcome-user/welcome-user.component';
 import { MyFlowerpotsComponent } from '../../../flowerpot/components/my-flowerpots/my-flowerpots.component';
 
 @Component({
   selector: 'app-flowerpots-list',
   standalone: true,
-  imports: [ToolbarComponent, MyFlowerpotsComponent],
+  imports: [ToolbarComponent, WelcomeUserComponent , MyFlowerpotsComponent],
   templateUrl: './flowerpots-list.component.html',
   styleUrl: './flowerpots-list.component.css'
 })
