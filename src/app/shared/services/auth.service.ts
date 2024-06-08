4599da355f819fba8fdd80e883a494d742842285
marzzioChicana <u202022228@upc.edu.pe>
2024-06-07 22:02:56 -0500

    add recover-password with api

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Auth, signOut } from '@angular/fire/auth';
+import { Auth, signOut, sendPasswordResetEmail } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private auth: Auth) { }
+  constructor(private router: Router, private auth: Auth) { }
+
+  forgotPassword(email: string) {
+    return sendPasswordResetEmail(this.auth, email).then(() => {
+      this.router.navigate(['/recover/password/confirmation', "valid"]);
+    }).catch((error) => { 
+      return error.message;
+    });
+  }
 
   logout() {
     return signOut(this.auth);
